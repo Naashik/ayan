ff9b35c5d81dfb21778b96626bd701636faa112e
unknown <naashiknazar@gmail.com>
2022-05-07 10:59:14 +0530

    latest commit

--- pages/index.vue
+++ pages/index.vue
@@ -89,13 +89,7 @@
                     <div class="section-title">Ayana Sea</div>
                     <p>Ayana Sea, is an exclusive boutique resort overlooking the Indian ocean and located in the charming fishing village of Wadduwa in the southwest coast of Sri Lanka, which is 33 kilometers from the city of Colombo. Ayana Sea brings together the best traits of a villa, providing privacy and personalized services, and the best traits of a hotel, providing facilities and space to deliver an experience like no other.</p>
                     <p>Secluded in 2 acres of carefully manicured lawns with coconut trees, expansive pools and koi fishponds, the Ocean Secrets Restaurant and the SALT bar with spacious and well equipped private units of two storied villas with en suite pools and terraces for a tranquil, pampered and an unforgettable experience.</p>
-                    <!-- call -->
-                    <div class="reservations">
-                        <div class="icon"><span class="flaticon-call"></span></div>
-                        <div class="text">
-                            <p>Reservation</p> <a href="tel:+94771131287 , +94382282885 , +94382282886 , +94767025396">+94771131287 , +94382282885 , +94382282886 , +94767025396</a>
-                        </div>
-                    </div>
+                    
                 </div>
                 <div class="col col-md-3 animate-box" data-animate-effect="fadeInUp">
                     <img src="img/overview-2.jfif" alt="" class="mt-90 mb-30">
@@ -453,57 +447,7 @@
         </div>
     </section>
     
- 
-    <!-- Reservation & Booking Form -->
-    <section class="testimonials">
-        <div class="background bg-img bg-fixed section-padding pb-0" data-background="img/slider/2.jpg" data-overlay-dark="2">
-            <div class="container">
-                <div class="row">
-                    <!-- Reservation -->
-                    <div class="col-md-5 mb-30 mt-30">
-                        <p><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i></p>
-                        <h5>Each of our guest rooms feature a private bath, wi-fi, cable television and include full breakfast.</h5>
-                        <div class="reservations mb-30">
-                            <div class="icon color-1"><span class="flaticon-call"></span></div>
-                            <div class="text">
-                                <p class="color-1">Reservation</p> <a class="color-1" href="tel:+94771131287 , +94382282885 , +94382282886 , +94767025396">+94771131287 , +94382282885 , +94382282886 , +94767025396</a>
-                            </div>
-                        </div>
-                        <p><i class="ti-check"></i><small>Call us, it's toll-free.</small></p>
-                    </div>
-                    <!-- Booking From -->
-                    
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Clients -->
-    <section class="clients">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7 owl-carousel owl-theme">
-                    <div class="clients-logo">
-                        <a href="#0"><img src="img/clients/1.png" alt=""></a>
-                    </div>
-                    <div class="clients-logo">
-                        <a href="#0"><img src="img/clients/2.png" alt=""></a>
-                    </div>
-                    <div class="clients-logo">
-                        <a href="#0"><img src="img/clients/3.png" alt=""></a>
-                    </div>
-                    <div class="clients-logo">
-                        <a href="#0"><img src="img/clients/4.png" alt=""></a>
-                    </div>
-                    <div class="clients-logo">
-                        <a href="#0"><img src="img/clients/5.png" alt=""></a>
-                    </div>
-                    <div class="clients-logo">
-                        <a href="#0"><img src="img/clients/6.png" alt=""></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+
    <footer class="footer">
         <div class="footer-top">
             <div class="container">
